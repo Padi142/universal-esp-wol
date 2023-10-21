7da3c199719c4b04893f4637772353a415e89015
Padias <matyas.krejza@seznam.cz>
2023-10-21 16:18:46 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+﻿
+# ESP32/ESP  Wake on LAN Telegram Bot
+
+Easy-to-setup Telegram bot, running on a cheap esp based microcontroller that can turn on your PC remotely!
+
+
+##  Libraries
+For this project to work, you need the following libraries:
+  - [WakeOnLan](https://www.arduino.cc/reference/en/libraries/wakeonlan/): for sending magic packets
+  - [UniversalTelegramBot](https://www.arduino.cc/reference/en/libraries/universaltelegrambot/): for using the Telegram API
+  - (*optional*) [ESP8266Ping](https://github.com/dancol90/ESP8266Ping) **OR** [ESP32Ping](https://github.com/marian-craciunescu/ESP32Ping) for checking the status of your device  ((Download as zip from Github and import them to ARDUINO IDE))
+
+
+Also ESP32/ESP8266 board libraries are needed. Add following lines to `Aditional boards urls` field in ARDUINO IDE:
+
+    http://arduino.esp8266.com/stable/package_esp8266com_index.json,https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+
+## Setup
+
+- Create a new Telegram bot and configure your `BOT_TOKEN` and `ALLOWED_ID`  
+  _You can use [@Botfather](https://t.me/botfather) to create a new bot and [@flop_flop_bot](https://t.me/flop_flop_bot) to get your ID_
+- Fill your _WiFi configuration_ and the _MAC address_ of the PC you want to turn on
+- (*optional*) Fill your local ip and uncomment few lines
+- Compile and flash
+
+##  Usage
+
+- Use `/start` to get a list of the available commands
+- Use the `/start_pc` command or press the physical button to turn on your PC
+- Use the `/ping` command to check if the bot is online
+- Use the `/is_on` command to check the status of your computer
+
+## CREDITS
+
+-  [daniele-salvagni's esp 32 WOL project](https://github.com/daniele-salvagni/wol-bot-esp32) 
+-  [Universal Telegram bot library for examples](https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot) 
---
+++ esp32/esp32.ino
@@ -0,0 +1,132 @@
+#include "WiFiMulti.h"
+#include "WiFiClientSecure.h"
+#include <WiFiUdp.h>
+
+#include "WakeOnLan.h"
+#include "UniversalTelegramBot.h"
+#include "ArduinoJson.h"
+
+//Library for pinging your machine
+//Uncomment if out want to use it
+#include <ESP32Ping.h>
+
+// Telegram Bot Token
+#define BOT_TOKEN  "0000000000:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#define ALLOWED_ID "000000000"
+
+// WiFi configuration
+#define WIFI_SSID ""
+#define WIFI_PASS ""
+
+// MAC address of the target device
+#define MAC_ADDR "FF:FF:FF:FF:FF:FF"
+
+//Local ip of you machine
+//You can leave this commented if you dont plan to use the /is_on command
+IPAddress local_ip (192, 168, 1, 0);
+
+
+WiFiMulti wifiMulti;
+WiFiClientSecure secured_client;
+WiFiUDP UDP;
+WakeOnLan WOL(UDP);
+
+const unsigned long BOT_MTBS = 1000; // mean time between scan messages
+UniversalTelegramBot bot(BOT_TOKEN, secured_client);
+unsigned long bot_lasttime; // last time messages' scan has been done
+
+void handleMessages(int numNewMessages) {
+  Serial.print("handling messages ");
+  Serial.println(numNewMessages);
+
+  for (int i = 0; i < numNewMessages; i++) {
+    Serial.println(bot.messages[i].from_id);
+    if (bot.messages[i].from_id != ALLOWED_ID) continue;
+    
+    String chat_id = bot.messages[i].chat_id;
+    String text = bot.messages[i].text;
+
+    if (text == "/start_pc") {
+      
+      WOL.sendMagicPacket(MAC_ADDR); 
+      delay(300);
+      bot.sendMessage(chat_id, "PC started! 🤩", "");
+
+    } else if (text == "/ping") {
+
+      bot.sendMessage(chat_id, "Pong.", "");
+
+    //This command pings you pc
+    //You can comment out this section from # to ##
+    //#
+    } else if(text == "/is_on"){
+
+      bot.sendMessage(chat_id, "Your PC seems to be: ", "");
+      bool status = Ping.ping(local_ip);
+
+      if(status){
+        bot.sendMessage(chat_id, "ON! ✅", "");
+      }else{
+        bot.sendMessage(chat_id, "OFF! ❌", "");
+      }
+    //##
+    }
+    else if (text == "/start") {
+
+      String welcome = "✨ Welcome to **WoL Bot** ✨ \n";
+      welcome += "/start_pc : Wakes your PC\n";
+      welcome += "/ping : Check the bot status\n";
+      welcome += "/is_on : Check status of your PC\n";
+      bot.sendMessage(chat_id, welcome, "Markdown");
+
+    }
+  }
+}
+
+
+
+void setup(){
+  // Configure WiFI
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASS);
+
+  // Add root certificate for api.telegram.org
+  secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT);
+
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+  Serial.print("...");
+  while ((wifiMulti.run() != WL_CONNECTED)) { // wait for WiFi connection.
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected");
+
+  // Attention: 255.255.255.255 is denied in some networks
+  WOL.calculateBroadcastAddress(WiFi.localIP(), WiFi.subnetMask()); 
+
+  configTime(0, 0, "pool.ntp.org"); // get UTC time via NTP
+  time_t now = time(nullptr);
+  while (now < 24 * 3600) {
+    Serial.print(".");
+    delay(150);
+    now = time(nullptr);
+  }
+  Serial.println(now);
+  
+}
+
+
+
+void loop() {
+  if (millis() - bot_lasttime > BOT_MTBS) {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    while (numNewMessages) {
+      Serial.println("Message received");
+      handleMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    bot_lasttime = millis();
+  }
+
+  delay(10);
+}
